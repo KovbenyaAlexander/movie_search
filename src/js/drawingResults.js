7ec707d4e88343c640e94885567edd77b93acf2c
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-06-28 21:12:50 +0300

    fix: implement correct drawing with a small number of results

--- src/js/drawingResults.js
+++ src/js/drawingResults.js
@@ -3,6 +3,7 @@ import createDomElement from "./createDomElement";
 
 export default function drawingResults(data) {
 
+
     const swiperWrapper = document.querySelector(".swiper-wrapper");
     swiperWrapper.innerHTML = "";
     for (let i = 0; i < 20; i++) {
@@ -14,20 +15,29 @@ export default function drawingResults(data) {
     for (let i = 0; i < 20; i++) {
         const swiperItem = document.querySelectorAll(`.slide_${i}`);
 
-        for (let item of swiperItem) {
-            item.setAttribute("pos", i);
+        if (i < data.results.length) {
+            for (let item of swiperItem) {
+                item.setAttribute("pos", i);
 
-            if (data.results[i].original_title) {
-                item.innerHTML = `<p class="name-of-movie">${data.results[i].original_title}</p>`;
-            } else {
-                item.innerHTML = "<p class=\"name-of-movie\">Original title not found</p>";
+                if (data.results[i].original_title) {
+                    item.innerHTML = `<p class="name-of-movie">${data.results[i].original_title}</p>`;
+                } else {
+                    item.innerHTML = "<p class=\"name-of-movie\">Original title not found</p>";
+                }
+
+                if (data.results[i].poster_path) {
+                    item.innerHTML += `<img src="${urlPoster}${data.results[i].poster_path}" width="100%" height="80%">`;
+                } else {
+                    item.innerHTML += "<img src=\"https://kritka.info/uploads/posts/no_poster.jpg\" width=\"100%\" height=\"80%\">";
+                }
+
             }
-
-            if (data.results[i].poster_path) {
-                item.innerHTML += `<img src="${urlPoster}${data.results[i].poster_path}" width="100%" height="80%">`;
-            } else {
-                item.innerHTML += `<img src="https://kritka.info/uploads/posts/no_poster.jpg" width="100%" height="80%">`;
+        } else {
+            for (let item of swiperItem) {
+                item.remove();
             }
         }
     }
+
+    swiper.update();
 }
