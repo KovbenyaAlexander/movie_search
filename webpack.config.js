128be648fc0d59d9113c80fe3c8d4526075a2169
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-07-03 23:03:52 +0300

    fix: slider bug. add local file swiper-bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: [
+        './src/js/swiper-bundle.js',
         './src/js/index.js',
         './src/scss/style.scss',
-        './node_modules/spin.js/spin.js',
-        './src/js/loader.js',
     ],
     output: {
         filename: './js/bundle.js'
